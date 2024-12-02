5babf30d212d04f94d281350c35483525faafeb2
DevClaudioFilho <devclaudiofilho@gmail.com>
2024-12-02 21:48:48 +0000

    update pymongo

---
+++ app/app/routers.py
@@ -0,0 +1,20 @@
+class MongoDBRouter:
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'ficha4':
+            return 'mongodb'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'ficha4':
+            return 'mongodb'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'ficha4' or obj2._meta.app_label == 'ficha4':
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label == 'ficha4':
+            return db == 'mongodb'
+        return None
